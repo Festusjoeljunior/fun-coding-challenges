3413da6ee0ebc25174f8ddbd5c3c7cd86d6d4f16
Festus Ifiti jr <festusjunior@gmail.com>
2023-06-13 14:56:56 +0100

    Two number sum solutions

--- src/sectionEasy/twoNumberSum/SolutionOne.java
+++ src/sectionEasy/twoNumberSum/SolutionOne.java
@@ -2,28 +2,5 @@ package sectionEasy.twoNumberSum;
 
 public class SolutionOne {
 
-    public static int[] twoNumberSum(int[] array, int targetSum) {
-        for(int i=0; i<array.length; i++) {
-            for(int j=1+1; j<array.length; j++) {
-                int firstNumber = array[i];
-                int secondNumber = array[j];
-                if (firstNumber + secondNumber == targetSum) {
-                    return new int[]{firstNumber, secondNumber};
-                }
-            }
-        }
-        return new int[0];
-    }
 
-    public static void main(String[] args) {
-        int[] array = {3,5,-4,8,11,1,-1,6};
-        int target = 10;
-        int[] result = twoNumberSum(array, target);
-
-        if (result.length == 0) {
-            System.out.println("No two numbers sum up to the target sum.");
-        } else {
-            System.out.println("Numbers found: " + result[0] + ", " + result[1]);
-        }
-    }
 }
--- src/sectionEasy/twoNumberSum/SolutionThree.java
+++ src/sectionEasy/twoNumberSum/SolutionThree.java
@@ -4,33 +4,5 @@ import java.util.Arrays;
 
 public class SolutionThree {
 
-    public static int[] twoNumberSum(int[] array, int targetSum) {
-        Arrays.sort(array);
-        int firstPointer = 0;
-        int secondPointer = array.length-1;
 
-        while (firstPointer < secondPointer) {
-            int currentSum = array[firstPointer] + array[secondPointer];
-            if (currentSum == targetSum) {
-                return new int[]{array[firstPointer], array[secondPointer]};
-            } else if (currentSum < targetSum) {
-                firstPointer++;
-            } else {
-                secondPointer--;
-            }
-        }
-        return new int[0];
-    }
-
-    public static void main(String[] args) {
-        int[] array = {3,5,-4,8,11,1,-1,6};
-        int target = 10;
-        int[] result = twoNumberSum(array, target);
-
-        if (result.length == 0) {
-            System.out.println("No two numbers sum up to the target sum.");
-        } else {
-            System.out.println("Numbers found: " + result[0] + ", " + result[1]);
-        }
-    }
 }
