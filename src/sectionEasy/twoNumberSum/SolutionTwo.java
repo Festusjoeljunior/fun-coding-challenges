b546de02ebd63d1ec206e6959f8a676eb1f76e30
Festus Ifiti jr <festusjunior@gmail.com>
2023-06-13 15:06:59 +0100

    adding solutions for two number sum

--- src/sectionEasy/twoNumberSum/SolutionTwo.java
+++ src/sectionEasy/twoNumberSum/SolutionTwo.java
@@ -1,6 +1,30 @@
 package sectionEasy.twoNumberSum;
 
+import java.util.Hashtable;
+
 public class SolutionTwo {
 
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        Hashtable<Integer, Integer> numbers = new Hashtable<>();
+        for(int i=0; i<array.length; i++) {
+            int complement = targetSum - array[i];
+            if (numbers.containsKey(complement)) {
+                return new int[]{complement, array[i]};
+            }
+            numbers.put(array[i], i);
+        }
+        return new int[0];
+    }
 
+    public static void main(String[] args) {
+        int[] array = {3,5,-4,8,11,1,-1,6};
+        int target = 10;
+        int[] result = twoNumberSum(array, target);
+
+        if (result.length == 0) {
+            System.out.println("No two numbers sum up to the target sum.");
+        } else {
+            System.out.println("Numbers found: " + result[0] + ", " + result[1]);
+        }
+    }
 }
